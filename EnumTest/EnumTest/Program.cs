e54b0c7cc8e3d73e45f4a135fd0f659be1a4d7ce
Eduardo Spaki <eduardo.spaki@farfetch.com>
2017-08-22 09:54:09 +0100

    first commit

---
+++ EnumTest/EnumTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EnumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ToString() of an Enum will return the description
+            // in this case "Friday"
+            Console.WriteLine(DayOfWeek.Friday.ToString());
+
+            // this will return the Enum number
+            Console.WriteLine((int)DayOfWeek.Friday);
+            var numberOfTheDayOfWeek = 5;
+
+            Console.WriteLine(numberOfTheDayOfWeek.ToEnum<DayOfWeek>());
+            var stringOfTheDayOfWeek = "Friday";
+
+            Console.WriteLine(stringOfTheDayOfWeek.ToEnum<DayOfWeek>());
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class EnumConversion
+    {
+        public static T ToEnum<T>(this string value) => (T)Enum.Parse(typeof(T), value);
+
+        public static T ToEnum<T>(this int value) => (T)Enum.ToObject(typeof(T), value);
+    }
+}
